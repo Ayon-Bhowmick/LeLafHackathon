c53fc2faf6f73c317da66e3cdc7141f43e9fb42b
Ayon Bhowmick <ayb224@lehigh.edu>
2023-11-11 17:37:26 -0500

    read barcode

---
+++ Lambda/processBarcode_test.py
@@ -0,0 +1,25 @@
+import cv2
+from pyzbar.pyzbar import decode
+
+cap = cv2.VideoCapture(0)
+
+def readBarcode(img):
+    barcodes = decode(img)
+    if not barcodes:
+        print("No barcode detected")
+    else:
+        for barcode in barcodes:
+            if barcode.data!="":
+                print(barcode.data)
+
+while 1:
+    ret, frame = cap.read()
+    if not ret:
+        print("Unable to capture video")
+        break
+    readBarcode(frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
