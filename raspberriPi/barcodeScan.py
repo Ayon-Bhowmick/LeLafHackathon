15fdda2ec7f8b9cff082466141c1c92d659f8975
Tiana Aldroubi <tianama02@gmail.com>
2023-11-11 23:50:18 -0500

    cant figure it out

--- raspberriPi/barcodeScan.py
+++ raspberriPi/barcodeScan.py
@@ -6,6 +6,6 @@ cam = cv2.VideoCapture(0)
 
 ret, image = cam.read()
 cv2.imshow('imagetest.jpg',image)
-cv2.imwrite('/test/imagetest.jpg', image)
+cv2.imwrite('imagetest.jpg', image)
 cam.release()
 cv2.destroyAllWindows()
