6ff84c146b8aa33f2819c61e78690417591f5d77
Ayon Bhowmick <ayb224@lehigh.edu>
2023-11-11 21:37:31 -0500

    made the zip

---
+++ Lambda/test/testRedis.py
@@ -0,0 +1,10 @@
+"""tests reading from redis"""
+import redis
+import os
+
+def lambda_handler(event, context):
+    redis_host = os.environ['REDIS_HOST']
+    redis_port = os.environ['REDIS_PORT']
+    r = redis.Redis(host=redis_host, port=redis_port)
+
+    print(r.get("my cached key"))
