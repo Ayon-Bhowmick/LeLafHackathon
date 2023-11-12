fb9dc736ffb72a5106879f172c39ad4f93e8c3eb
Tiana Aldroubi <tianama02@gmail.com>
2023-11-12 06:01:51 -0500

    rm clone

--- raspberriPi/iot/start.sh
+++ raspberriPi/iot/start.sh
@@ -15,21 +15,21 @@ if [ ! -f ./root-CA.crt ]; then
 fi
 
 # Check to see if AWS Device SDK for Python exists, download if not
-if [ ! -d ./aws_iot_device_sdk_python_v2 ]; then
-  printf "\nCloning the AWS SDK...\n"
-  git clone https://github.com/aws/aws_iot_device_sdk_python_v2.git --recursive
-fi
+# if [ ! -d ./aws_iot_device_sdk_python_v2 ]; then
+#   printf "\nCloning the AWS SDK...\n"
+#   git clone https://github.com/aws/aws_iot_device_sdk_python_v2.git --recursive
+# fi
 
 # Check to see if AWS Device SDK for Python is already installed, install if not
-if ! python3 -c "import awsiot" &> /dev/null; then
-  printf "\nInstalling AWS SDK...\n"
-  python3 -m pip install ./aws_iot_device_sdk_python_v2
-  result=$?
-  if [ $result -ne 0 ]; then
-    printf "\nERROR: Failed to install SDK.\n"
-    exit $result
-  fi
-fi
+# if ! python3 -c "import awsiot" &> /dev/null; then
+#   printf "\nInstalling AWS SDK...\n"
+#   python3 -m pip install ./aws_iot_device_sdk_python_v2
+#   result=$?
+#   if [ $result -ne 0 ]; then
+#     printf "\nERROR: Failed to install SDK.\n"
+#     exit $result
+#   fi
+# fi
 
 # run pub/sub sample app using certificates downloaded in package
 printf "\nRunning pub/sub sample application...\n"
