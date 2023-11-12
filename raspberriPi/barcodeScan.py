ff58cffb92c66c3b8066914deec726aaa1307d0a
Tiana Aldroubi <tianama02@gmail.com>
2023-11-11 23:46:12 -0500

    aaaah '

--- raspberriPi/barcodeScan.py
+++ raspberriPi/barcodeScan.py
@@ -5,7 +5,7 @@ import cv2
 cam = cv2.VideoCapture(0)
 
 ret, image = cam.read()
-cv2.imshow('Imagetest',image)
+cv2.imshow('Imagetest.jpg',image)
 cv2.imwrite('/test', image)
 cam.release()
 cv2.destroyAllWindows()
